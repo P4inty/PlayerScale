b4331b49f1bb24996f9cce2f7e3e26dc17b1b77e
Martin Wundsam <martin.wundsam@gmail.com>
2022-01-04 13:22:43 +0100

    Add project files.

---
+++ Client/Client.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace Client
+{
+    public class Client : BaseScript
+    {
+        public Client()
+        {
+            EventHandlers.Add("onClientResourceStart", new Action<string>(OnClientResourceStart));
+            EventHandlers.Add("client:scale:invalid", new Action(InvalidScale));
+            EventHandlers.Add("client:scale:valid", new Action<double>(ValidScale));
+        }
+
+        private void OnClientResourceStart(string resourceName)
+        {
+            if (GetCurrentResourceName() == resourceName) return;
+
+            RegisterCommand("scale", new Action<int, List<object>>(Scale), false);
+        }
+
+        private void Scale(int source, List<object> args)
+        {
+            if (!double.TryParse(args[0].ToString(), out var scale))
+            {
+                Message("Der Skalierungswert muss eine Komma Zahl sein (z.B 0.9).");
+                return;
+            }
+            TriggerServerEvent("server:scale",source, scale);
+        }
+
+        private void InvalidScale()
+        {
+            Message("Der Skalierungswert muss zwischen 0.5 und 1.0 liegen.");
+        }
+
+        private void ValidScale(double scale)
+        {
+            SetPedScale(PlayerPedId(), (float)scale);
+        }
+
+        private void Message(string message)
+        {
+            TriggerEvent("chat:addMessage", new
+            {
+                color = new[] {255, 0 , 0},
+                args = new[] {"[Scaling] " + message}
+            });
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+
+namespace Server
+{
+    public class Server : BaseScript
+    {
+        public Server()
+        {
+           EventHandlers.Add("server:scale", new Action<int, double>(Scale));
+        }
+
+        private void Scale(int source, double value)
+        {
+            Debug.WriteLine(Players.ElementAt(source).Name + " tried to scale to: " + value);
+            if (value < 0.5 || value > 1.0)
+            {
+                TriggerClientEvent(Players.ElementAt(source), "client:scale:invalid");
+                return;
+            }
+            TriggerClientEvent("client:scale:valid", value);
+        }
+    }
+}
